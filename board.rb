eb2d9bb8e99653e6e2e98be310668e64150c3db1
App Academy Student <student@appacademy.io>
2014-01-09 12:53:09 -0500

    Initializes board properly, sets up perform_slide and perform_jump

--- board.rb
+++ board.rb
@@ -1,48 +1,82 @@
+require "./piece.rb"
 class Board
-
+  attr_accessor :board
   def initialize
-    @board = Array.new(8){ Array.new(8){ nil } }
+    @grid = Array.new(8){ Array.new(8){ nil } }
     setup_red
     setup_white
   end
 
   def [](pos)
     x,y = pos
-    @board[y][x]
+    @grid[y][x]
   end
 
   def []=(pos, value)
     x,y = pos
-    @board[y][x] = value
+    @grid[y][x] = value
   end
 
   def setup_red
     (0..2).each do |y|
       if y % 2 == 0
         (0..3).each do |x|
-          @board[x*2+1][y] = Piece.new([x*2+1,y], :red)
+          pos = [x * 2 + 1, y]
+          self[pos]= Piece.new([x * 2 + 1, y], self, :red)
+          #@board[x*2+1,y] = Piece.new([x*2+1,y], :red)
+          #@board[y][x*2+1] =Piece.new([x*2+1,y], :red)
         end
       else
         (0..3).each do |x|
-          @board[x*2][y] = Piece.new([x*2+1,y], :red)
+          pos = [x * 2, y]
+          self[pos] = Piece.new([x * 2, y], self, :red)
+         # @board[x*2,y] = Piece.new([x*2+1,y], :red)
+          #@board[y][x*2] =Piece.new([x*2,y], :red)
         end
       end
     end
+    nil
   end
 
   def setup_white
     (5..7).each do |y|
       if y % 2 == 0
         (0..3).each do |x|
-          @board[x*2][y] = Piece.new([x*2+1,y], :white)
+          pos = [x * 2, y]
+          self[pos] = Piece.new([x*2,y], self, :white)
         end
       else
         (0..3).each do |x|
-          @board[x*2+1][y] = Piece.new([x*2+1,y], :white)
+          pos = [x * 2 + 1, y]
+          self[pos] = Piece.new([x * 2 + 1, y], self, :white)
         end
       end
     end
+    nil
   end
 
+  def render
 
+    @grid.each_with_index do |row, y|
+      row.each_with_index do |value, x|
+        if value.nil?
+          print "_ "
+        elsif value.color == :red
+          print "R "
+        else
+          print "W "
+        end
+
+      end
+      puts
+    end
+    nil
+  end
+
+  def move (start, stop)
+    piece = self[start]
+    self[stop] = piece
+    piece = nil
+
+  end
 end
--- piece.rb
+++ piece.rb
@@ -1,13 +1,62 @@
 class Piece
-  attr_accessor :king
+  attr_accessor :position, :king, :color
 
-  def initialize(color)
-    @king = false
+  def initialize(position, board, color)
+    @position = position
+    @board = board
     @color = color
+    @king = false
   end
 
-  def perform_slide
+  def perform_slide(stop)
+
+    possible_positions = []
+    deltas = move_diffs
+    deltas.each do |delta|
+      possible_position = [@position[0]+delta[0], @position[1]+delta[1]]
+      if is_valid?(possible_position)
+        possible_positions << possible_position
+      end
+    end
+
+    if possible_positions.include?(stop)
+      @position = stop
+      @board.move(@position, stop)
+      maybe_promote?
+      return true
+    end
+
+    false
+  end
+
+  def perform_jump(stop)
+
 
   end
 
+  def move_diffs
+    if @king
+      [ [1, -1], [-1, -1], [1, 1], [-1, 1] ]
+    elsif @color == :white
+      [ [1, -1], [-1, -1] ]
+    else
+      [[1, 1], [-1, 1] ]
+    end
+  end
+
+  def is_valid?(position)
+    x, y = position
+    if @board[position].nil? && x.between?(0, 7) && y.between?(0, 7)
+      return true
+    end
+    false
+  end
+
+  def maybe_promote?
+    if @color == :white && @position[1] == 0
+      @king = true
+    elsif @color == :black && @position[1] == 7
+      @king = true
+    end
+  end
 end
