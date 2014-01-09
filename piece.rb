7b761c133cce10698cfbcb0e5696fc8b01e196e8
App Academy Student <student@appacademy.io>
2014-01-09 13:48:35 -0500

    Fixes captured piece not being removed

--- piece.rb
+++ piece.rb
@@ -11,8 +11,9 @@ class Piece
 =begin
   load 'board.rb'
   b=Board.new
-  b[[1,2]].perform_slide([2,3])
-  b[[3,2]].perform_jump([1,4])
+  b[[2,2]].perform_slide([3,3])
+  b[[1,5]].perform_slide([2,4])
+  b[[3,3]].perform_jump([1,5])
 
 =end
   def perform_slide(stop)
@@ -42,24 +43,31 @@ class Piece
     jump_deltas = move_diffs_jump
     slide_deltas = move_diffs_slide
     curr_x, curr_y = @position
+
     jump_deltas.each_with_index do |delta, index|
       possible_position = [curr_x+delta[0], curr_y+delta[1]]
       adjacent_x, adjacent_y = slide_deltas[index]
       middle_position=[curr_x+adjacent_x, curr_y+adjacent_y]
-      if @board[middle_position].nil?
-      elsif is_valid?(possible_position) && @board[[curr_x+adjacent_x, curr_y+adjacent_y]].color != @color
+
+      if (!@board[middle_position].nil?) && (is_valid?(possible_position)) && (@board[middle_position].color != @color)
         possible_positions << possible_position
       end
     end
 
     if possible_positions.include?(stop)
+      middle_piece_position = get_average(@position, stop)
       @board[stop]=self
       @board[@position]=nil
       @position = stop
+      @board[middle_piece_position] = nil
       maybe_promote?
       return true
     end
     false
+  end
+
+  def get_average(start, stop)
+     [(start[0] + stop[0])/2, (start[1] + stop[1])/2]
   end
 
   def move_diffs_slide
